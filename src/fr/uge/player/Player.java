013f50ad1672d50d6eb445db748ec6138ece463a
Aramisotos <yanis77860@gmail.com>
2024-11-11 18:02:04 +0100

    fix: ajout de requireNoNUll sur les méthodes public de la class Player

--- src/fr/uge/player/Player.java
+++ src/fr/uge/player/Player.java
@@ -5,16 +5,20 @@ import fr.uge.tile.Tile;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Objects;
 
 public class Player {
     private final DepartTile departTile;
     private final Map<Tile, HashSet<Tile>> tileNeighborMap = new HashMap<>();
 
     public Player(DepartTile departTile) {
+        Objects.requireNonNull(departTile);
         this.departTile = departTile;
     }
 
     public void add(Tile tile, Tile neighbor) {
+        Objects.requireNonNull(tile);
+        Objects.requireNonNull(neighbor);
         tileNeighborMap.computeIfAbsent(tile, _ -> new HashSet<>()).add(neighbor);
     }
 }
