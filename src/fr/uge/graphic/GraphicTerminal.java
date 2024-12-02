300265b501b59d4f0d79358d015a8063d7f03ae4
Aramisotos <yanis77860@gmail.com>
2024-12-02 19:38:35 +0100

    fix : déplacement du code prévu pour les interactions entre le joueur et le jeu

--- src/fr/uge/graphic/GraphicTerminal.java
+++ src/fr/uge/graphic/GraphicTerminal.java
@@ -35,39 +35,6 @@ public class GraphicTerminal implements Graphic {
     return sb.toString();
   }
 
-  public static void choosePutPileOnPlayerBoard(Set<Tile> tiles) {
-    return;
-  }
-
-  public static OptionTileToken ChoicePlayer(List<OptionTileToken> optionTileTokenList) {
-    return null;
-  }
-
-  public static String choosePutTileToken() {
-    System.out.println("Choose with put tile or token first");
-    var scanner = new Scanner(System.in);
-    var input = scanner.nextLine().trim().toLowerCase();
-    if (input.equals("tiles") || input.equals("token")) {
-      return input;
-    }
-    System.out.println("Invalid input ! Please enter a valid choice  : tile or token.");
-    return choosePutTileToken();
-  }
-  //TODO a faire
-  public static Tile choosePutToken(List<Tile> tiles) {
-    return null;
-  }
-
-  // TODO a finir
-  public static List<Tile> choosePutTile(List<Tile> tiles) {
-    System.out.println("Choose a tile to put on the board");
-    var scanner = new Scanner(System.in);
-    var input = scanner.nextLine().trim().toLowerCase();
-    System.out.println("Invalid input ! Please enter a valid tile.");
-    //choosePutTile(tiles);
-    return null;
-  }
-
   @Override
   public void drawGameBoard() {
 
--- src/fr/uge/interaction/TerminalInteraction.java
+++ src/fr/uge/interaction/TerminalInteraction.java
@@ -1,3 +1,43 @@
 package fr.uge.interaction;
 
-public class TerminalInteraction implements Interaction{}
+import fr.uge.game.OptionTileToken;
+import fr.uge.tile.Tile;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+
+public class TerminalInteraction implements Interaction {
+  public static void choosePutPileOnPlayerBoard(Set<Tile> tiles) {
+    return;
+  }
+
+  public OptionTileToken ChoicePlayer(List<OptionTileToken> optionTileTokenList) {
+    return null;
+  }
+
+  public String choosePutTileToken() {
+    System.out.println("Choose with put tile or token first");
+    var scanner = new Scanner(System.in);
+    var input = scanner.nextLine().trim().toLowerCase();
+    if (input.equals("tiles") || input.equals("token")) {
+      return input;
+    }
+    System.out.println("Invalid input ! Please enter a valid choice  : tile or token.");
+    return choosePutTileToken();
+  }
+
+  // TODO a faire
+  public Tile choosePutToken(List<Tile> tiles) {
+    return null;
+  }
+
+  // TODO a finir
+  public List<Tile> choosePutTile(List<Tile> tiles) {
+    System.out.println("Choose a tile to put on the board");
+    var scanner = new Scanner(System.in);
+    var input = scanner.nextLine().trim().toLowerCase();
+    System.out.println("Invalid input ! Please enter a valid tile.");
+    // choosePutTile(tiles);
+    return null;
+  }
+}
