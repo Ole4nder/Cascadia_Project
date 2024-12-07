436788b834815e7a34417fe10fc4c4deee221497
Aramisotos <yanis77860@gmail.com>
2024-12-07 16:09:19 +0100

    add : ajout de l'enum pour les landscape d'une tile

---
+++ src/fr/uge/tile/TileLandscape.java
@@ -0,0 +1,32 @@
+package fr.uge.tile;
+
+import fr.uge.animal.Animals;
+
+public enum TileLandscape {
+    RIVER("river"),
+    SWAMP("swamp"),
+    MOUNTAIN("mountain"),
+    FOREST("forest"),
+    PASTURE("pasture"),
+    DEFAULT("default")
+    ;
+    private final String landscape;
+    TileLandscape(String landscape) {
+        this.landscape = landscape;
+    }
+    public String getLandscape() {
+        return landscape;
+    }
+
+    public static TileLandscape landscapeNameToEnums(String landscape) {
+        return switch (landscape) {
+            case "rivières" -> TileLandscape.RIVER;
+            case "marais" -> TileLandscape.SWAMP;
+            case "montagnes" -> TileLandscape.MOUNTAIN;
+            case "forêts" -> TileLandscape.FOREST;
+            case "prairies" -> TileLandscape.PASTURE;
+            case "default" -> TileLandscape.DEFAULT;
+            default -> throw new IllegalStateException("Unexpected value: " + landscape);
+        };
+    }
+}
