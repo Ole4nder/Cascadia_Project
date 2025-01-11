13b133dd0e4c584259da6efc4a8a4a4602f9d009
Aramisotos <yanis77860@gmail.com>
2025-01-11 22:38:57 +0100

    fix : fix build.xml !!!

--- src/fr/uge/main/Main.java
+++ src/fr/uge/main/Main.java
@@ -26,7 +26,7 @@ public class Main {
     };
   }
 
-  public static void main() {
+  public static void main(String[] args) {
     Application.run(
         Color.DARK_GRAY,
         applicationCOntext -> {
